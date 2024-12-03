2676ff9a4357a661fff16d61520295f2fe3f9122
Void <youssefmagdyanwar@yandex.ru>
2024-12-03 23:19:46 +0300

    added origin cors ( methods - allowed headers - credentials)

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      process.env.CLIENT_URL,
-    ],
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
 
